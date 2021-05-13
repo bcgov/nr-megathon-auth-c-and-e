bc3faff2a3327e0e640f14d326917557003d18e0
Meredith Mumby <meredith.mumby@gmail.com>
2021-05-13 15:24:35 -0700

    updated the navbar to include links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import AuthorizationMap from "./AuthorizationMap";
 function App() {
   return (
     <div className="App">
-      <nav class="navbar fixed-top navbar-dark bg-primary">
+      {/* <nav class="navbar fixed-top navbar-dark bg-primary">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
             <a href="https://gov.bc.ca/">
@@ -13,6 +13,74 @@ function App() {
             </a>
             The Back Yard
           </a>
+
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link disabled"
+                  href="#"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav> */}
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a href="https://gov.bc.ca/">
+            <img alt="BC Government Logo" src={LOGO} width={154} />
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Map
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Reporting
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
       <AuthorizationMap />
